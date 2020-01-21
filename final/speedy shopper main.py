ed9ed059c77c02cd7efddd47f82dce6670b8dea8
Gron1275 <grennongurney@gmail.com>
2020-01-21 17:54:23 -0500

    Update speedy shopper main.py

--- final/speedy shopper main.py
+++ final/speedy shopper main.py
@@ -1,5 +1,4 @@
-import time
-import os
+
 import wx
 
 grocery_list = []
@@ -34,83 +33,95 @@ def initialize(file):
     aisles_full = aisles
 
 
-class Output(wx.Frame):
+class OtherFrame(wx.Frame):
 
-    def __init__(self):
-        wx.Frame.__init__(self, None, id=wx.ID_ANY, title=u'Output Frame', pos=wx.DefaultPosition, size=wx.Size(500, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+    def __init__(self, title, parent=None):
+        wx.Frame.__init__(self, parent=parent, title=title)
+        search(grocery_list)
+        print(requested_aisles_dict)
+
         self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
-        bSizer1 = wx.BoxSizer(wx.VERTICAL)
-        bSizer2 = wx.BoxSizer(wx.VERTICAL)
-        self.wait_pls = wx.StaticText(self, wx.ID_ANY, u"Gathering Information", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.wait_pls.Wrap(-1)
-        bSizer2.Add(self.wait_pls, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-        self.gauge = wx.Gauge(self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL)
-        self.gauge.SetValue(0)
-        bSizer2.Add(self.gauge, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
-        bSizer1.Add(bSizer2, 1, wx.ALIGN_CENTER | wx.ALL | wx.EXPAND, 5)
-        self.SetSizer(bSizer1)
+
+        full_box = wx.BoxSizer(wx.VERTICAL)
+
+        self.first_output = wx.StaticText(self, wx.ID_ANY, u"Aisles Requested", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.first_output.Wrap(-1)
+
+        full_box.Add(self.first_output, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
+        self.SetSizer(full_box)
         self.Layout()
+
         self.Centre(wx.BOTH)
 
+        self.Show()
 
-class Home(wx.Frame):
+
+class MainFrame(wx.Frame):
+
     global grocery_list
 
     def __init__(self):
-        wx.Frame.__init__(self, None, id=wx.ID_ANY, title=u'speedy shop', pos=wx.DefaultPosition, size=wx.Size(500, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+        wx.Frame.__init__(self, None, id=wx.ID_ANY, title=u"Speedy Shopper Program", pos=wx.DefaultPosition, size=wx.Size(500, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+
         self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
-        bSizer1 = wx.BoxSizer(wx.VERTICAL)
-        bSizer2 = wx.BoxSizer(wx.VERTICAL)
-        self.welcome = wx.StaticText(self, wx.ID_ANY, u"Welcome to speedy shopper program", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.welcome.Wrap(-1)
-        bSizer2.Add(self.welcome, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-        bSizer3 = wx.BoxSizer(wx.VERTICAL)
-        self.instructions = wx.StaticText(self, wx.ID_ANY, u"Add Items to Grocery List", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.instructions.Wrap(-1)
-        bSizer3.Add(self.instructions, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-        self.text_box = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer3.Add(self.text_box, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        full_box = wx.BoxSizer(wx.VERTICAL)
+
+        welcome_box = wx.BoxSizer(wx.VERTICAL)
+
+        self.m_staticText1 = wx.StaticText(self, wx.ID_ANY, u"Welcome to speedy shopper program", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText1.Wrap(-1)
+
+        welcome_box.Add(self.m_staticText1, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        input_box = wx.BoxSizer(wx.VERTICAL)
+
+        self.add_items_help = wx.StaticText(self, wx.ID_ANY, u"Add Items to Grocery List", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.add_items_help.Wrap(-1)
+
+        input_box.Add(self.add_items_help, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.text_input = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        input_box.Add(self.text_input, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
         self.append_button = wx.Button(self, wx.ID_ANY, u"Add To List", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer3.Add(self.append_button, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+        input_box.Add(self.append_button, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
         self.search_button = wx.Button(self, wx.ID_ANY, u"Search Grocery List", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer3.Add(self.search_button, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-        bSizer2.Add(bSizer3, 1, wx.ALIGN_CENTER | wx.EXPAND, 5)
-        bSizer1.Add(bSizer2, 1, wx.ALIGN_CENTER | wx.ALL | wx.EXPAND, 5)
-        self.SetSizer(bSizer1)
+        input_box.Add(self.search_button, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        welcome_box.Add(input_box, 1, wx.ALIGN_CENTER | wx.EXPAND, 5)
+
+        full_box.Add(welcome_box, 1, wx.ALIGN_CENTER | wx.ALL | wx.EXPAND, 5)
+
+        self.SetSizer(full_box)
         self.Layout()
+
         self.Centre(wx.BOTH)
-        # Connect Events
-        self.append_button.Bind(wx.EVT_BUTTON, self.append_grocery)
-        self.text_box.Bind(wx.EVT_TEXT_ENTER, self.append_grocery)
-        self.search_button.Bind(wx.EVT_BUTTON, self.search)
 
         # Connect Events
+        self.text_input.Bind(wx.EVT_TEXT_ENTER, self.append_grocery)
+        self.append_button.Bind(wx.EVT_BUTTON, self.append_grocery)
+        self.search_button.Bind(wx.EVT_BUTTON, self.search)
+
+        self.Show()
 
     def __del__(self):
         pass
 
-    # Virtual event handlers, override them in your derived class
-
-    def load_outFrame(self):
-
-        wx.Frame.Close(self)
-
+    # Virtual event handlers, overide them in your derived class
     def append_grocery(self, event):
-        item = self.text_box.GetLineText(0)
+        item = self.text_input.GetLineText(0)
         grocery_list.append(item.lower())
-        self.text_box.Clear()
+        self.text_input.Clear()
 
     def search(self, event):
-        # self.frame.Show()
-        search(grocery_list)
-        wx.Exit()
-
-    def onClose(self, event):
+        input_frame = OtherFrame(title="Input")
         self.Close()
 
 
 def scan_reg(item):
-    print("Running: " + "[PID: " + str(os.getpid()) + "]")
     for grocery in aisles_full:
         if grocery == item:
             print(grocery)
@@ -118,27 +129,18 @@ def scan_reg(item):
             if aisles_full[grocery] not in requested_aisles_dict:  # take this out for multiple items in aisle
                 requested_aisles_dict[aisles_full[grocery]] = []
             (requested_aisles_dict[aisles_full[grocery]]).append(item)
-    print("Finished: " + "[PID: " + str(os.getpid()) + "]")
 
 
 def search(items):
-    time.sleep(0.5)
-    output = Output()
-    global time_completed
-    start_time = time.time()
     for i in items:
         scan_reg(i)
-        
-    time_completed = time.time() - start_time
 
 
 if __name__ == '__main__':
 
     initialize('item_locations.txt')
-    app = wx.App()
-    frame1 = Home()
-    frame1.Show()
-    # frame2 = Output().Show()
+    app = wx.App(False)
+    frame = MainFrame()
     app.MainLoop()
 
     print(requested_aisles_dict)  # Print what item from grocery list is in the aisle
