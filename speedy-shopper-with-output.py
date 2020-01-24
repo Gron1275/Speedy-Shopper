4c0df2be6d2624d7dfd938f3a13ecfbd774d51e6
erikaingersoll <34252106+erikaingersoll@users.noreply.github.com>
2020-01-24 12:34:17 -0500

    Update speedy-shopper-with-output.py

--- speedy-shopper-with-output.py
+++ speedy-shopper-with-output.py
@@ -1,17 +1,5 @@
-import time
-import os
-
 import wx
-
-
-
-
-import wx
-
 grocery_list = []
-
-time_completed = 0
-requested_aisles = []
 requested_aisles_dict = {}
 aisles = {}
 aisles_full = {}
@@ -53,77 +41,62 @@ def initialize(file):
 
 
 class OtherFrame(wx.Frame):
+    global grocery_list
 
     def __init__(self, title, parent=None):
         wx.Frame.__init__(self, parent=parent, title=title)
-        print(requested_aisles_dict)
         search(grocery_list)
-        print(requested_aisles_dict)
-
+        font_size = 15
         self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
 
         full_box = wx.BoxSizer(wx.VERTICAL)
 
         self.first_output = wx.StaticText(self, wx.ID_ANY, u"Aisles Requested", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.first_output.SetFont(wx.Font(font_size, wx.DEFAULT, wx.NORMAL, wx.BOLD))
         self.first_output.Wrap(-1)
-        default_pos_x = 5
-        counter = 1
-        if produce != ["Produce:"]:
-            for a in produce:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if bakery != ["Bakery:"]:
-            for a in bakery:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if deli != ["Deli:"]:
-            for a in deli:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if back != ['Back:']:
-            for a in back:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if meat != ["Meat:"]:
-            for a in meat:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if seafood != ["Seafood:"]:
-            for a in seafood:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        for i in range(1, 17):
-            if globals()['req_aisle%s' % i] != [f'Aisle {i}' + ":"]:
-                for a in globals()['req_aisle%s' % i]:
-                    self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20),
-                                                       wx.Size(200, 200), 0)
-                    counter = counter + 1
-        if dairy != ["Dairy:"]:
-            for a in dairy:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if hbc != ["Health and Body Care:"]:
-            for a in hbc:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if pharmacy != ["Pharmacy:"]:
-            for a in pharmacy:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
-        if front != ["Front:"]:
-            for a in front:
-                self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, a, wx.Point(default_pos_x, counter * 20), wx.Size(200, 200), 0)
-                counter = counter + 1
 
+        X_POSITION = 5
+        y_determinate = 1
+        grid = wx.GridSizer(0, 2, 0, 0)
+        for key in requested_aisles_dict:
+
+            globals()['self.%s' % key] = wx.StaticText(self, wx.ID_ANY, u"{}".format(str(key).title() + ':'), wx.Point(X_POSITION, y_determinate * 10), wx.DefaultSize, 0)
+            globals()['self.%s' % str(key) + " items"] = wx.StaticText(self, wx.ID_ANY, u"{}".format(str(', '.join(requested_aisles_dict[key]).title())), wx.Point(X_POSITION, y_determinate * 10), wx.DefaultSize, 0)
+
+            globals()['self.%s' % key].Wrap(-1)
+            globals()['self.%s' % str(key) + " items"].Wrap(-1)
+
+            grid.Add(globals()['self.%s' % key], 0, wx.ALL, 5)
+            grid.Add(globals()['self.%s' % str(key) + " items"], 0, wx.ALL, 5)
+
+            y_determinate += 1
+
+        self.new_searchbutton = wx.Button(self, wx.ID_ANY, u"New Search", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.close_button = wx.Button(self, wx.ID_ANY, u"Close Program", wx.DefaultPosition, wx.DefaultSize, 0)
 
         full_box.Add(self.first_output, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+        full_box.Add(grid, 0, wx.ALIGN_CENTER_HORIZONTAL, 5)
+        full_box.Add(self.new_searchbutton, 0, wx.ALIGN_BOTTOM | wx.ALIGN_CENTER | wx.ALL, 5)
+        full_box.Add(self.close_button, 0, wx.ALIGN_BOTTOM | wx.ALIGN_CENTER | wx.ALL, 5)
 
         self.SetSizer(full_box)
         self.Layout()
-
         self.Centre(wx.BOTH)
 
+        self.new_searchbutton.Bind(wx.EVT_BUTTON, self.new_search)
+        self.close_button.Bind(wx.EVT_BUTTON, self.close_app)
+
         self.Show()
+
+
+
+    def new_search(self, event):
+        grocery_list.clear()
+        frame = MainFrame()
+        self.Close()
+
+    def close_app(self, event):
+        self.Close()
 
 
 class MainFrame(wx.Frame):
@@ -139,12 +112,14 @@ class MainFrame(wx.Frame):
 
         welcome_box = wx.BoxSizer(wx.VERTICAL)
 
+        input_box = wx.BoxSizer(wx.VERTICAL)
+
+        self.help_button = wx.Button(self, wx.ID_ANY, u"Help", wx.Point(434, 0), wx.Size(50,25), 0)
+
         self.m_staticText1 = wx.StaticText(self, wx.ID_ANY, u"Welcome to speedy shopper program", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText1.Wrap(-1)
 
         welcome_box.Add(self.m_staticText1, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-
-        input_box = wx.BoxSizer(wx.VERTICAL)
 
         self.add_items_help = wx.StaticText(self, wx.ID_ANY, u"Add Items to Grocery List", wx.DefaultPosition, wx.DefaultSize, 0)
         self.add_items_help.Wrap(-1)
@@ -156,6 +131,9 @@ class MainFrame(wx.Frame):
 
         self.append_button = wx.Button(self, wx.ID_ANY, u"Add To List", wx.DefaultPosition, wx.DefaultSize, 0)
         input_box.Add(self.append_button, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+
+        self.clear_list = wx.Button(self, wx.ID_ANY, u"Clear Grocery List", wx.DefaultPosition, wx.DefaultSize, 0)
+        input_box.Add(self.clear_list, 0, wx.ALIGN_CENTER | wx.ALL, 5)
 
         self.search_button = wx.Button(self, wx.ID_ANY, u"Search Grocery List", wx.DefaultPosition, wx.DefaultSize, 0)
         input_box.Add(self.search_button, 0, wx.ALIGN_CENTER | wx.ALL, 5)
@@ -172,7 +150,11 @@ class MainFrame(wx.Frame):
         # Connect Events
         self.text_input.Bind(wx.EVT_TEXT_ENTER, self.append_grocery)
         self.append_button.Bind(wx.EVT_BUTTON, self.append_grocery)
+        self.clear_list.Bind(wx.EVT_BUTTON, self.clear_grocerylist)
         self.search_button.Bind(wx.EVT_BUTTON, self.search)
+        self.help_button.Bind(wx.EVT_BUTTON, self.helpwin(self, full_box, welcome_box, input_box))
+
+
 
         self.Show()
 
@@ -186,85 +168,44 @@ class MainFrame(wx.Frame):
         self.text_input.Clear()
 
     def search(self, event):
-        input_frame = OtherFrame(title="Input")
+        input_frame = OtherFrame(title="Requested Aisles")
         self.Close()
 
+    def clear_grocerylist(self, event):
+        grocery_list.clear()
 
+    def helpwin(self, event, full_box, welcome_box, input_box):
+        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+        self.full_box.Hide()
+        self.welcome_box.Hide()
+        self.input_box.Hide()
+        bSizer1 = wx.BoxSizer(wx.VERTICAL)
 
-def scan_reg(item):
-    for grocery in aisles_full:
-        if grocery == item:
-            print(grocery)
-            print(aisles_full[grocery] + " From [reg]")
-            if aisles_full[grocery] not in requested_aisles_dict:  # take this out for multiple items in aisle
-                requested_aisles_dict[aisles_full[grocery]] = []
-            (requested_aisles_dict[aisles_full[grocery]]).append(item)
+        self.m_staticText1 = wx.StaticText(self, wx.ID_ANY, u"asdasda", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText1.Wrap(-1)
 
+        bSizer1.Add(self.m_staticText1, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALL, 5)
+
+        self.SetSizer(bSizer1)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
+
+    def __del__( self ):
+        pass
 
 def search(items):
-    for i in items:
-        scan_reg(i)
-    sort_aisles()
+    for item in items:
+        for grocery in aisles_full:
+            if grocery == item:
+                if aisles_full[grocery] not in requested_aisles_dict:
+                    requested_aisles_dict[aisles_full[grocery]] = set()
+                (requested_aisles_dict[aisles_full[grocery]]).add(item)
 
-for i in range(1,17):
-    globals()['req_aisle%s' % i] = [f'Aisle {i}' + ":"]
-hbc = ["Health and Body Care:"]
-back = ["Back:"]
-front = ["Front:"]
-pharmacy = ["Pharmacy:"]
-produce = ["Produce:"]
-bakery = ["Bakery:"]
-deli = ["Deli:"]
-meat = ["Meat:"]
-seafood = ["Seafood:"]
-dairy = ["Dairy:"]
-def sort_aisles():
-
-    for item in requested_aisles_dict.keys():
-        for i in range(1, 17):
-            if item == f'aisle {i}':
-                for x in requested_aisles_dict[item]:
-                    globals()['req_aisle%s' % i].append(requested_aisles_dict[item][requested_aisles_dict[item].index(x)])
-        if item == "aisle hbc":
-            for i in requested_aisles_dict[item]:
-                hbc.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle back":
-            for i in requested_aisles_dict[item]:
-                back.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle front":
-            for i in requested_aisles_dict[item]:
-                front.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle pharm":
-            for i in requested_aisles_dict[item]:
-                pharmacy.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle produce":
-            for i in requested_aisles_dict[item]:
-                produce.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle bakery":
-            for i in requested_aisles_dict[item]:
-                bakery.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle deli":
-            for i in requested_aisles_dict[item]:
-                deli.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle meat":
-            for i in requested_aisles_dict[item]:
-                meat.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle seafood":
-            for i in requested_aisles_dict[item]:
-                seafood.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
-        elif item == "aisle dairy":
-            for i in requested_aisles_dict[item]:
-                dairy.append(requested_aisles_dict[item][requested_aisles_dict[item].index(i)])
 
 if __name__ == '__main__':
 
     initialize('item_locations.txt')
-
     app = wx.App(False)
     frame = MainFrame()
     app.MainLoop()
-
-    search(grocery_list)
-    print(requested_aisles_dict)  # Print what item from grocery list is in the aisle
-
-    print("Time Completed: " + str(time_completed))
